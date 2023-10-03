56127336e3c5001d7c491c7424b5c9ed34754d73
ferkrodriguez98 <ferkrodriguez98@gmail.com>
2023-10-03 12:43:47 -0300

    Configure STATIC_ROOT

--- tesis_horoscopo/settings.py
+++ tesis_horoscopo/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app_tesis/static/css'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
